fcc82fd5d7bd03ffb58b76d627d8e0f3e2aec426
Ben Hudson <ben@hudsonsathome.com>
2024-09-19 11:10:09 -0400

    option to use pretrained model in baseline script

--- scripts/pyepo_baseline.py
+++ scripts/pyepo_baseline.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import pathlib
 import pyepo
 import pyepo.metric
 import torch
@@ -9,14 +11,19 @@ from data.pyepo import PyEPODataset, gen_shortestpath_data
 from ignite.metrics import Average
 from models.parallel_solver import ParallelSolver
 from models.risk_averse import CVaRShortestPath
-from utils import get_val_metrics, get_wandb_name, WandBHistogram
+from models.solver_vae import SolverVAE
+from utils import get_eval_metrics, get_wandb_name, WandBHistogram, norm_normal
 
 
 def get_argparser():
     parser = argparse.ArgumentParser("Run a baseline on a dataset")
 
+    parser.add_argument(
+        "--config", type=pathlib.Path, help="WandB experiment config to reproduce (additional arguments override)"
+    )
+
     dataset_args = parser.add_argument_group("dataset", description="Dataset arguments")
-    dataset_args.add_argument("dataset", type=str, choices=["shortestpath", "portfolio"], help="Dataset to generate")
+    dataset_args.add_argument("--dataset", type=str, choices=["shortestpath", "portfolio"], help="Dataset to generate")
     dataset_args.add_argument("--n_samples", type=int, default=2000, help="Number of samples to generate")
     dataset_args.add_argument("--n_features", type=int, default=5, help="Number of features")
     dataset_args.add_argument("--degree", type=int, default=1, help="Polynomial degree for encoding function")
@@ -26,7 +33,13 @@ def get_argparser():
 
     model_args = parser.add_argument_group("model", description="Model arguments")
     model_args.add_argument(
-        "baseline", type=str, choices=["oracle", "random", "mean", "ra_oracle"], help="Baseline to evaluate"
+        "--model", type=str, choices=["oracle", "random", "mean", "pretrained"], help="Prediction model"
+    )
+    model_args.add_argument("--weights", type=pathlib.Path, help="Weights for pretrained prediction model")
+
+    eval_args = parser.add_argument_group("evaluation", description="Evaluation arguments")
+    eval_args.add_argument(
+        "--risk_level", type=float, default=0.5, help="Risk level (probability) for risk-averse decision-making"
     )
 
     train_args = parser.add_argument_group("training", description="Training arguments")
@@ -37,6 +50,16 @@ def get_argparser():
     model_args.add_argument("--wandb_tags", type=str, nargs="+", default=[], help="WandB tags")
 
     return parser
+
+
+def set_args_from_config(args, config):
+    args_dict = vars(args)
+    for arg, value in args_dict.items():
+        if arg in config and argparser.get_default(arg) == value:
+            new_value = config[arg]["value"]
+            print(f"setting {arg}={new_value} from config")
+            args_dict[arg] = new_value
+    return args
 
 
 def report_metrics(metrics: dict, step: int, use_wandb: bool):
@@ -56,16 +79,24 @@ def report_metrics(metrics: dict, step: int, use_wandb: bool):
 if __name__ == "__main__":
     argparser = get_argparser()
     args = argparser.parse_args()
+    if args.config is not None:
+        config = json.load(open(args.config))
+        args = set_args_from_config(args, config)
 
     args.use_wandb = args.wandb_project is not None
     if args.use_wandb:
         import wandb
 
+        if args.model is not None:
+            tags = ["experiment"]
+        elif args.baseline is not None:
+            tags = ["baseline"]
+
         run = wandb.init(
             project=args.wandb_project,
             config=args,
             name=get_wandb_name(args, argparser),
-            tags=["baseline"] + args.wandb_tags,
+            tags=tags + args.wandb_tags,
         )
 
     torch.manual_seed(args.seed)
@@ -92,13 +123,25 @@ if __name__ == "__main__":
         dataset, batch_size=batch_size, shuffle=False, num_workers=args.workers, drop_last=False
     )
 
-    cost_marginal_mean = costs.mean(dim=0)
-    cost_marginal_std = costs.std(dim=0, correction=0)
+    if args.model == "pretrained":
+        assert args.config is not None, "--config must be specified when model=pretrained"
+        assert args.weights is not None, "--weights must be specified when model=pretrained"
 
-    if args.baseline != "ra_oracle":
+        feats, costs, sols, objs, cost_dist_params = dataset[0]
+        model = SolverVAE(
+            feats.size(-1),
+            sols.size(-1),
+            costs.size(-1),
+            config["mlp_hidden_dim"]["value"],
+            config["mlp_hidden_layers"]["value"],
+        )
+        model.load_state_dict(torch.load(args.weights, map_location="cpu", weights_only=True))
+        model.eval()
+
+    if args.risk_level == 0.5:
         solver = ParallelSolver(args.workers, pyepo.model.grb.shortestPathModel, grid)
     else:
-        solver = ParallelSolver(args.workers, CVaRShortestPath, grid, 0.99, tail="right")
+        solver = ParallelSolver(args.workers, CVaRShortestPath, grid, args.risk_level, tail="right")
 
     metrics = defaultdict(Average)
     # override these
@@ -112,21 +155,32 @@ if __name__ == "__main__":
         cost_dist_mean, cost_dist_std = cost_dist_params.chunk(2, dim=-1)
         cost_dist = torch.distributions.Normal(cost_dist_mean, cost_dist_std)
 
-        if args.baseline == "ra_oracle":
-            y_pred = cost_dist_params
+        if args.model == "pretrained":
+            with torch.no_grad():
+                feats_normed, _, _, _, _ = dataset.norm(feats=feats)
+                prior = model.sample(feats_normed)
+                prior_normed = norm_normal(prior)
+                if args.risk_level == 0.5:
+                    y_pred = prior_normed.loc
+                else:
+                    y_pred = torch.cat([prior_normed.loc, prior_normed.scale], dim=-1)
+                costs_pred = prior_normed.loc
+
+        elif args.model == "oracle":
+            if args.risk_level == 0.5:
+                y_pred = cost_dist_mean
+            else:
+                y_pred = cost_dist_params
             costs_pred = cost_dist_mean
-        elif args.baseline == "oracle":
-            y_pred = costs_pred = cost_dist_mean
-        elif args.baseline == "random":
-            y_pred = costs_pred = cost_marginal_mean + cost_marginal_std * torch.randn_like(costs)
-        elif args.baseline == "mean":
-            y_pred = costs_pred = cost_marginal_mean.expand_as(costs)
+
+        else:
+            raise ValueError(f"unknown baseline {args.baseline}")
 
         sols_pred = solver(y_pred)
 
         objs_pred = torch.bmm(costs_pred.unsqueeze(1), sols_pred.unsqueeze(2)).squeeze(2)
 
-        eval_metrics = get_val_metrics(
+        eval_metrics = get_eval_metrics(
             costs,
             sols,
             objs,
